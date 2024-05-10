c8c54a8ec0ead4254e478a3621f0dae0b665d4ab
oscardenas <oscardenas17@gmail.com>
2024-05-10 16:56:17 -0500

    Create Project -controller-router  - validacion body

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
+import { body } from "express-validator";
+import { handleInputsErrors } from "../middleware/validation";
 
 const router = Router();
 
+router.post(
+  "/",
+  body("projectName")
+    .notEmpty()
+    .withMessage("El nombre del Proyecto es Obligatorio"),
+  body("clienteName")
+    .notEmpty()
+    .withMessage("El nombre del cliente es Obligatorio"),
+  body("description").notEmpty().withMessage("La descripción es Obligatoria"),
+  handleInputsErrors,
+  ProjectController.createProject
+);
 router.get("/", ProjectController.getAllProjects);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ dotenv.config();
 connectDB();
 const app = express();
 
+app.use(express.json());
+
 //Routes
 app.use("/api/projects/", projectRoutes);
 
